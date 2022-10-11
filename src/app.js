9c1226f1c1d6c69c6f5412dba6c04b297fead261
hiago <hiago.tech@gmail.com>
2022-10-11 07:38:45 -0300

    route refactoring

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const controller = require('./controller');
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
@@ -7,15 +9,20 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const contract = await controller.getContract(id, req.profile);
+
+        if(!contract) return res.status(404).end();
+
+        res.status(200).json(contract);
+    } catch(error) {
+        console.log('ERROR: ', error);
+        return res.status(500).send('Internal Server Error');
+    }
+});
+
+
 module.exports = app;
